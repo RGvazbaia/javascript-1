bfa32ad42e93990da1a8c61865fa1150438798f3
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-11-29 14:57:29 +0400

    add javascript

---
+++ script.js
@@ -0,0 +1,111 @@
+// davaleba 1
+
+
+// let user={
+//    username:'giorgi',
+//    lastname:"smith",
+//    age:25,
+//    studentstatus:"active"
+// };
+
+// console.log(user.studentstatus);
+
+
+
+// davaleba 2
+
+// let numbers=[5,25,17,30,23];
+// for(let numb of numbers){
+//     if(numb>5){
+//         console.log(numb);
+//     }
+// }
+
+
+
+// davaleba 3
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for(let numb of numbers)
+// if (numb>5){
+//     console.log(numb);
+// }
+
+
+// davaleba 4 
+
+// let user = {
+//    name: 'giorgi',
+//    age:  20,
+//    studentstatus: 'active'
+// }
+
+
+
+// if ((user.age < 18) && (user.studentstatus == active)){
+//    console.log('Hello');
+// }
+// else {console.log('Error');}
+
+// if (user.name == 'levani'){
+//    console.log('Hello levani')
+// }
+// else {console.log('Error')}
+
+// if ((user.userstatus=='active') || (user.age<25)) {
+//    console.log('Hello World')
+// }
+// else {console.log('Error')}
+
+
+//  davaleba 5
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+// for (let string of array){
+//    if (typeof string === "string" ){
+//       console.log(string)
+//    }
+// }
+
+
+// davaleba 6
+
+// let numbers = [ -5, 22, -10, 20, 15, -8 ];
+
+// for (let x = 0; x < numbers.length; x++) {
+//    if (numbers[x] > 0) {
+//       console.log(numbers[x])
+
+//    }
+// }
+
+// davaleba 7
+// let array = [ 2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10 ];
+
+
+// for(let x of array){
+//    if(x % 2 == 0){
+//       console.log(x)
+//    }
+// }
+
+// for(let x of array){
+//    if(x % 2 == 1){
+//       console.log(x)
+//    }
+// }
+
+
+// davaleba 8
+
+
+// let users = [
+//    {username: 'giorgi', status: false},
+//    {username: 'levani', status: false},
+//    {username: 'anna', status: true}
+// ]
+
+// for(let x of users){
+//    if(x.status==true){
+//       console.log(x)
+//    }
+// }
